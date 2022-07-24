8e31d4e1b8e365286c28d0cd7605b956cf739a46
GitHub Action <action@github.com>
2022-07-24 08:32:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/learningsupa/kanbantasks/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/learningsupa/kanbantasks/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.learningsupa.kanbantasks.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.learningsupa.kanbantasks.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/learningsupa/kanbantasks/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/learningsupa/kanbantasks/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.learningsupa.kanbantasks.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.learningsupa.kanbantasks.MyBundle
 
 class MyApplicationService {
 
